6e78d7781cd0ca85b85f90150d0d7bc952568a0b
KryEv <Kry.Ev.Ed@yandex.ru>
2025-10-24 21:04:33 +0700

    commit_1

--- main_test.go
+++ main_test.go
@@ -48,3 +48,80 @@ func TestCafeWhenOk(t *testing.T) {
 		assert.Equal(t, http.StatusOK, response.Code)
 	}
 }
+
+// тест на количества записей
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		request string
+		countOk int
+	}{
+		// варианты проверки
+		{"/cafe?city=moscow&count=0", 0},
+		{"/cafe?city=tula&count=0", 0},
+		{"/cafe?city=moscow&count=1", 1},
+		{"/cafe?city=tula&count=1", 1},
+		{"/cafe?city=moscow&count=2", 2},
+		{"/cafe?city=tula&count=2", 2},
+		{"/cafe?city=moscow&count=100", len(cafeList["moscow"])},
+		{"/cafe?city=tula&count=100", len(cafeList["tula"])},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", v.request, nil)
+		handler.ServeHTTP(response, req)
+
+		// конвертируем ответ
+		countResp := strings.Split(response.Body.String(), ",")
+
+		// для случая с 0
+		if (len(countResp) == 1) && (countResp[0] == "") {
+
+			assert.Equal(t, v.countOk, 0)
+			return
+		}
+
+		// для всех вариантов которые > 0
+		assert.Equal(t, v.countOk, len(countResp))
+
+	}
+}
+
+// тест на параметр поиска
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		search    string
+		wantCount int
+	}{
+		// варианты проверки
+		{"фасоль", 0},
+		{"кофе", 2},
+		{"вилка", 1},
+		{"", len(cafeList["moscow"])},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/cafe?city=moscow&search="+v.search, nil)
+		handler.ServeHTTP(response, req)
+
+		// конвертируем ответ
+		cafeSearch := strings.Split(response.Body.String(), ",")
+
+		found := 0
+
+		// проверяем ответ на количество совпадений
+		for _, cafe := range cafeSearch {
+			if strings.Contains(strings.ToLower(cafe), strings.ToLower(v.search)) {
+				found++
+			}
+		}
+
+		assert.Equal(t, v.wantCount, found)
+
+	}
+}
