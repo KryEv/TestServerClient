619da8d52f2b0a8eadd6c9c3d5b1fd7fc5ca95a8
KryEv <Kry.Ev.Ed@yandex.ru>
2025-10-25 15:09:45 +0700

    commit_2

--- main_test.go
+++ main_test.go
@@ -64,8 +64,8 @@ func TestCafeCount(t *testing.T) {
 		{"/cafe?city=tula&count=1", 1},
 		{"/cafe?city=moscow&count=2", 2},
 		{"/cafe?city=tula&count=2", 2},
-		{"/cafe?city=moscow&count=100", len(cafeList["moscow"])},
-		{"/cafe?city=tula&count=100", len(cafeList["tula"])},
+		{"/cafe?city=moscow&count=100", min(len(cafeList["moscow"]), 100)},
+		{"/cafe?city=tula&count=100", min(len(cafeList["tula"]), 100)},
 	}
 
 	for _, v := range requests {
@@ -73,18 +73,20 @@ func TestCafeCount(t *testing.T) {
 		req := httptest.NewRequest("GET", v.request, nil)
 		handler.ServeHTTP(response, req)
 
+		// проверка кода ответа
+		assert.Equal(t, http.StatusOK, response.Code)
+
 		// конвертируем ответ
 		countResp := strings.Split(response.Body.String(), ",")
 
 		// для случая с 0
 		if (len(countResp) == 1) && (countResp[0] == "") {
-
 			assert.Equal(t, v.countOk, 0)
 			return
 		}
 
 		// для всех вариантов которые > 0
-		assert.Equal(t, v.countOk, len(countResp))
+		assert.Len(t, countResp, v.countOk)
 
 	}
 }
@@ -109,19 +111,29 @@ func TestCafeSearch(t *testing.T) {
 		req := httptest.NewRequest("GET", "/cafe?city=moscow&search="+v.search, nil)
 		handler.ServeHTTP(response, req)
 
+		// проверка кода ответа
+		assert.Equal(t, http.StatusOK, response.Code)
+
 		// конвертируем ответ
 		cafeSearch := strings.Split(response.Body.String(), ",")
 
-		found := 0
-
-		// проверяем ответ на количество совпадений
-		for _, cafe := range cafeSearch {
-			if strings.Contains(strings.ToLower(cafe), strings.ToLower(v.search)) {
-				found++
-			}
+		// когда поиск не вернул ответ
+		if (len(cafeSearch) == 1) && (cafeSearch[0] == "") {
+			assert.Equal(t, v.wantCount, len(cafeSearch)-1)
+			return
 		}
 
-		assert.Equal(t, v.wantCount, found)
+		// проверяем ожидаемое количество ответов
+		assert.Equal(t, v.wantCount, len(cafeSearch))
+
+		// приводим полученный ответ к строчному регистру
+		for _, cafe := range cafeSearch {
+			cafeSearch = append(cafeSearch, strings.ToLower(cafe))
+		}
+
+		// проверяем полученный ответ с ожидаемым
+		assert.Contains(t, cafeSearch, strings.ToLower(v.search))
 
 	}
+
 }
